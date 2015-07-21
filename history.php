86216570ab354f14075289338caa9045374d9c75
Michael Stifter <michael.stifter2@edu.fh-joanneum.at>
2015-07-21 12:36:24 +0200

    Fixed error when no songs were played on the selected date

--- history.php
+++ history.php
@@ -10,29 +10,35 @@
 		
 		$songs = $mc->getMDB()->getPlayedHistoryForDate($date);
 		
-		$html .= "<h4><strong>Played songs on " . $mysql_date->convert2AustrianDate() . "</strong></h4>";
+		if (!empty($songs)) {
+			$html .= "<h4><strong>Played songs on " . $mysql_date->convert2AustrianDate() . "</strong></h4>";
 		
-		$html .= "<table class='table table-striped'>";
-			$html .= "<thead>";
-				$html .= "<tr>";
-					$html .= "<th>Time</th>";
-					$html .= "<th>Song</th>";
-					$html .= "<th>Artist</th>";
-					$html .= "<th>Record</th>";
-				$html .= "</tr>";
-			$html .= "</thead>";
-			
-			$html .= "<tbody>";
-				foreach ($songs as $song) {
+			$html .= "<table class='table table-striped'>";
+				$html .= "<thead>";
 					$html .= "<tr>";
-						$html .= "<td>" . getTimeFromTimestamp($song["Timestamp"]) . "</td>";
-						$html .= "<td>" . $song["SongName"] . "</td>";
-						$html .= "<td>" . $song["ArtistName"] . "</td>";
-						$html .= "<td>" . $song["RecordName"] . "</td>";
+						$html .= "<th>Time</th>";
+						$html .= "<th>Song</th>";
+						$html .= "<th>Artist</th>";
+						$html .= "<th>Record</th>";
 					$html .= "</tr>";
-				}
-			$html .= "</tbody>";
-		$html .= "</table>";
+				$html .= "</thead>";
+				
+				$html .= "<tbody>";
+					foreach ($songs as $song) {
+						$html .= "<tr>";
+							$html .= "<td>" . getTimeFromTimestamp($song["Timestamp"]) . "</td>";
+							$html .= "<td>" . $song["SongName"] . "</td>";
+							$html .= "<td>" . $song["ArtistName"] . "</td>";
+							$html .= "<td>" . $song["RecordName"] . "</td>";
+						$html .= "</tr>";
+					}
+				$html .= "</tbody>";
+			$html .= "</table>";
+		} else {
+			$html .= "<h4><strong>No songs were played on " . $mysql_date->convert2AustrianDate() . "</strong></h4>";
+		}
+		
+		
 	}
 	
 	echo $mc->getIndexHTML($html);
