61774b5ebefa000ed3b341fcf216197bd31ca16d
MICSTI <michael.stifter2@edu.fh-joanneum.at>
2015-08-26 14:14:55 +0200

    Added basic containers to custom range statistics page

--- custom_range.php
+++ custom_range.php
@@ -6,4 +6,13 @@
 	// headline
 	$html .= "<h3>Custom range statistics</h3>";
 	
+	// top bar with two datepickers and submit button
+	$html .= "<div class='custom-statistics-control'>";
+	$html .= "</div>";
+	
+	// result content div
+	$html .= "<div id='custom-statistics-result'>";
+		
+	$html .= "</div>";
+	
 	echo $mc->getIndexHTML($html, "statistics");
